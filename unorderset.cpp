d35d66ec4105577d570bd6ba49b84e7d5dd995a0
Khaled-Kamal <KhaledKamal1555@gmil.com>
2025-01-19 14:35:11 +0200

    Templates using in Problem Solving

---
+++ unorderset.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+using namespace std;
+
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> orderedSet;
+
+int main() {
+
+    orderedSet s;
+    s.insert(3);
+    s.insert(4);
+    s.insert(1);
+    s.insert(5);
+    s.insert(1);
+
+    s.erase(5);
+
+    for (auto it: s)
+        cout << it << " ";
+    // 1 3 4
+
+    return 0;
+}
